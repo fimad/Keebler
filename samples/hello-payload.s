26616b5ebb002d77819419889343cf0b98f401a7
Will Whitfield <willcoster@gmail.com>
2012-09-25 23:34:53 -0700

    Updated hello-payload.s

--- samples/hello-payload.s
+++ samples/hello-payload.s
@@ -11,16 +11,19 @@ _start:
   #print to stdout
   movq $1, %rbx
   #8 character long string
-  movq $8, %rdx
+  movq $13, %rdx
 
   #place our "hello!!\n" string on the stack
-  movq $0x0a21216f6c6c6548, %rcx
+  movq $0x0000000a21646c72, %rcx
+  pushq %rcx
+  movq $0x6f57206f6c6c6548, %rcx
   pushq %rcx
   movq %rsp, %rsi
 
   syscall
 
   #remove the string
+  pop %rcx
   pop %rcx
 
   pop %rbx
